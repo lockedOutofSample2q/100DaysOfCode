870d22ce27e150601a107dc46dcd4724f3bf8eec
Hargunraj Singh <hargunrajs@gmail.com>
2026-03-05 03:56:03 +0530

    Update Day8 - CeasarCypher.py

--- Day8 - CeasarCypher.py
+++ Day8 - CeasarCypher.py
@@ -13,8 +13,11 @@ def ceasar_encrypt(text_to_encrypt, ceasar_num):
     new_alpha = caesar_array(ceasar_num)
     encrypted_text=""
     for i in text_to_encrypt:
-        alphabet_index = alphabet.index(i)
-        encrypted_text= encrypted_text + new_alpha[alphabet_index]
+        if i not in alphabet:
+            encrypted_text = encrypted_text + i
+        else:
+            alphabet_index = alphabet.index(i)
+            encrypted_text= encrypted_text + new_alpha[alphabet_index]
     print("Original text: " + text_to_encrypt)
     print("Encrypted text: " + encrypted_text)
     return encrypted_text
@@ -24,8 +27,11 @@ encrypted_text = ceasar_encrypt(text_to_encrypt, ceasar_num)
 def ceasar_decrypt(encrypted_text, decryption_num):
     decrypted_text =""
     for i in encrypted_text:
-        alphabet_index = alphabet.index(i)
-        decrypted_text = decrypted_text + alphabet[alphabet_index - decryption_num]
+        if i not in alphabet:
+            decrypted_text = decrypted_text + i
+        else:
+            alphabet_index = alphabet.index(i)
+            decrypted_text = decrypted_text + alphabet[alphabet_index - decryption_num]
     print("Decrypted text: " + decrypted_text)
 
 decryption_num = input("Choose the decryption amount: ")
