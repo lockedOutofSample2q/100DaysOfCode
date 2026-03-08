627a8c9a2b24a3326e011abdf311755647016d6d
Hargunraj Singh <hargunrajs@gmail.com>
2026-03-08 22:52:40 +0530

    Add files via upload

---
+++ Day9 - Bidder.py
@@ -0,0 +1,21 @@
+
+bidding_dictionary={
+
+}
+
+more= "yes"
+
+while more == "yes":
+    ask_name = input("What's your name?: ")
+    ask_bid = input("What's your bid amount?: ")
+    bidding_dictionary[ask_name]= ask_bid
+    more = input("Are there more players? yes/no  : ").lower()
+
+
+highest_bid = 0
+for name in bidding_dictionary:
+    value = int(bidding_dictionary[name]) 
+    if value > highest_bid:
+        highest_bid = value
+        winner = name
+print("winner is : " + winner + " with a bid of $" + str(highest_bid))
